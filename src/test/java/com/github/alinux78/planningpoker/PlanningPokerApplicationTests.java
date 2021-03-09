45925b090b0656b6eafad42855f13577080c226f
Mihai Casu <mihai.casu@gmail.com>
2021-03-09 12:56:37 +0200

    init project

---
+++ src/test/java/com/github/alinux78/planningpoker/PlanningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alinux78.planningpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
